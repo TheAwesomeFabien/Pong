486ce86df868bd6eb89820f94c30ae0cc4cd9c71
Fabién Kouwenberg <fabienko266@gmail.com>
2020-11-27 12:30:46 +0100

    eerste push

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Vector2 velocity { get; private set; }
+
+    [SerializeField] private float moveSpeed = 12f;
+    [SerializeField] private float maxBounceAngle = 45f;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        velocity =Vector2.left * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Paddle")
+        {
+            BounceFromPaddle(collision.collider);   
+        }
+        else
+        { 
+            Bounce(); 
+        }
+        
+    }
+
+    private void Bounce()
+    {
+        velocity = new Vector2(velocity.x, -velocity.y);
+    }
+
+    private void BounceFromPaddle(Collider2D collider)
+    {
+        float colYExtent = collider.bounds.extents.y;
+        float yOffset = transform.position.y - collider.transform.position.y;
+
+        float yRatio = yOffset / colYExtent;
+        float bounceAngle = maxBounceAngle * yRatio * Mathf.Deg2Rad;
+        
+        Vector2 bounceDirection = new Vector2(Mathf.Cos(bounceAngle), Mathf.Sin(bounceAngle));
+
+        bounceDirection.x *= Mathf.Sign(-velocity.x);
+        
+        velocity = bounceDirection * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    public bool isAI;
+    private Ball ball;
+    private BoxCollider2D col;
+
+    private float randomYOffset;
+
+    private Vector2 forwardDirection;
+    private bool firstIncoming;
+    
+    public enum Side { Left, Right }
+
+    [SerializeField] private Side side;
+
+    private void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Ball>();
+        col = GetComponent<BoxCollider2D>();
+        if (side == Side.Left)
+        {
+            forwardDirection = Vector2.right;
+            
+        } else if (side == Side.Right) 
+        {
+            forwardDirection = Vector2.left; 
+        }
+    }
+
+    private void Update()
+    {
+        float targetYPosition = GetNewYPostion();
+        
+        ClampPosition(ref targetYPosition);
+
+        transform.position = new Vector3(transform.position.x, targetYPosition, transform.position.z);
+    }
+
+    private void ClampPosition(ref float yPosition)
+    {
+        float minY = Camera.main.ScreenToWorldPoint(new Vector3(0, 0)).y;
+        float maxY = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height)).y;
+
+        yPosition = Mathf.Clamp(yPosition, minY, maxY);
+    }
+
+    private float GetNewYPostion()
+    {
+        float result = transform.position.y;
+        
+        if (isAI)
+        {
+            if (BallIncoming())
+            {
+                if (firstIncoming)
+                {
+                    print("First time incoming");
+                    firstIncoming = false;
+                    randomYOffset = GetRandomOffset();
+                }
+
+                result = Mathf.MoveTowards(transform.position.y, ball.transform.position.y + randomYOffset, moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                firstIncoming = true;
+            }
+        }
+        else
+        {
+            float movement = Input.GetAxisRaw("Vertical")* moveSpeed * Time.deltaTime;
+            result = transform.position.y + movement;
+        }
+
+        return result;
+    }
+
+    private bool BallIncoming()
+    {
+        float dotP = Vector2.Dot(ball.velocity, forwardDirection);
+        return dotP < 0f;
+    }
+
+    private float GetRandomOffset()
+    {
+        float maxOffset = col.bounds.extents.y;
+        return Random.Range(-maxOffset, maxOffset);
+    }
+}
